a8dc5039a46f308cd530d382dc3c06c434381729
Dohyun Moon <d6moon@uwaterloo.ca>
2022-03-05 18:05:15 -0500

    vue config changed

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/dohyunSite/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/dohyunSite/index.html" : "/",
 };
